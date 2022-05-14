84c3a68386e1fc0ab0375a8e5eb10f33c25c5a91
zabarullah <60830173+zabarullah@users.noreply.github.com>
2022-05-14 12:46:52 +0100

    completed the functionality of the cart-dropdown, adding product images,description, quantity(cart.context) and added styling to it. Also updated the cart-icon with quanity

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,17 @@
-import './product-card.styles.scss';
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
 
 import Button from '../button/button.component';
 
+import './product-card.styles.scss';
+
+
 const ProductCard = ({ product }) => {
     const { name, price, imageUrl } = product;
+    const { addItemToCart } = useContext(CartContext);                          // Grab the addItemToCart function from cartContext, so that it can be used when the button is clicked
+
+    const addProductToCart = () => addItemToCart(product);                      // pass in the product to the function addItemToCart, this function will run when the button is clicked
+
     return (
     <div className='product-card-container'>
         <img src={imageUrl} alt={`${name}`} />
@@ -11,7 +19,7 @@ const ProductCard = ({ product }) => {
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
-        <Button buttonType='inverted'>Add to Cart</Button>
+        <Button buttonType='inverted' onClick={addProductToCart}>Add to Cart</Button>
     </div>  
     );  
 };
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,13 +1,41 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+
+const addCartItem = (cartItems, productToAdd) => {
+    //find if cartItems already contains the productToAdd  
+    const existingCartItem = cartItems.find((cartItem) => cartItem.id === productToAdd.id);
+
+    //if existingCartItem returns true, check again if cartItem's id matches with productToAdd's id, if matches then return a new array but also add 1 to quantity for that cartItem, otherwise, just return the cartItem 
+    if (existingCartItem) {
+        return cartItems.map((cartItem) => cartItem.id === productToAdd.id ? {...cartItem, quantity: cartItem.quantity + 1 } : cartItem);
+    }
+    // return new array with modified cartItems/new cart item
+    return [...cartItems, { ...productToAdd, quantity: 1 }];
+} 
 
 export const CartContext = createContext({
     isCartOpen: false,
-    setIsCartOpen: () => {}
+    setIsCartOpen: () => {},
+    cartItems:[],
+    addItemToCart: () => {},
+    cartCount: 0
 });
 
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
-    const value = { isCartOpen, setIsCartOpen };
+    const [cartItems, setCartItems] = useState([]);
+    const [cartCount, setCartCount] = useState(0);
+
+    // will keep track of the quantity of items in the cart
+    useEffect(() => {
+        const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
+        setCartCount(newCartCount);        
+    },[cartItems])
+
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems, productToAdd));
+    }
+
+    const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount };
     return(
         <CartContext.Provider value={value}>{children}</CartContext.Provider>
     )
