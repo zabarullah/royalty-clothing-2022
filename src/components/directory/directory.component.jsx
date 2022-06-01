a2f90a7d5057bec88ad623300bce6f13dc7856b5
zabarullah <60830173+zabarullah@users.noreply.github.com>
2022-06-01 13:12:41 +0100

    There was a clash in styles due to same classname being used by two components. Renamed category-item.component.jsx to directory-item.component.jsx + renamed component itself to DirectoryItem and updated it inside Directory Component. Changed the classes names in the component and styles sheet appropriately to fix the clash

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,4 +1,4 @@
-import CategoryItem from '../category-item/category-item.component';
+import DirectoryItem from '../directory-item/directory-item.component';
 
 import './directory.styles.scss'
 
@@ -6,7 +6,7 @@ const Directory = ({categories}) => {
     return (
         <div className="directory-container">
         {categories.map((category) => (
-          <CategoryItem key={category.id} category={category} />                   /*When mapping through the categories array it will assign the id for each category to key and category will be the mapped item */ 
+          <DirectoryItem key={category.id} category={category} />                   /*When mapping through the categories array it will assign the id for each category to key and category will be the mapped item */ 
   
         ))}
       </div>
