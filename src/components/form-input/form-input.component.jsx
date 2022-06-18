edcba9ccc7ecd9525e451d779ad4db9694dd939c
zabarullah <60830173+zabarullah@users.noreply.github.com>
2022-06-18 16:04:52 +0100

    created Form input styled components(Group, Input & FormInputLable, used CSS block to creak shrinkLableStyles variable, so it can be passed in as a props for the styled components, added formInput shrinking condition inside FormInputLable styled component. used variables for subColor & mainColor

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,12 +1,13 @@
-import './form-input.styles.scss';
+import { FormInputLable, Input, Group }from './form-input.styles';
 
 const FormInput = ({label, ...otherProps}) => {
    return (
-    <div className="group">
-        <input className="form-input" {...otherProps} />
+    <Group>
+        <Input {...otherProps} />
         { label && (                                                                                         //if label exists then render the label in the ( ) 
-        <label className={`${otherProps.value.length ? 'shrink' : null} form-input-label`}> {label} </label>)}  {/* if there is a value typed in it(otherProps.value.length) then add class shrink otherwise null, where it wont do anything. Besides this, there will always be a class form-input-label */}
-    </div>
+        <FormInputLable shrink={otherProps.value.length}> {label} </FormInputLable>)}   
+    </Group>
+        // if otherProps have any value(true) then this boolean will be passed on as prop otherwise false will be passed on 
    ); 
 };
 
