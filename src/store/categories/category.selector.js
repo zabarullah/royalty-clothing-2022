88b1cb99e79d2cfbb9310a167e30167254368f6d
zabarullah <60830173+zabarullah@users.noreply.github.com>
2022-08-28 16:34:57 +0100

    use reselect library to create selectors inside the category.selector.js - Using createSelector it will now only re-render a component if any of the arguments for any selector changes, otherwise, the previous state remains the same. console logs here best seen in action when we go to the hats page signed in and later clicking signout, this will show the the select 1 fromthe category.selector fires but selector 2 and 3 did not fire.

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,8 +1,26 @@
-export const selectCategoriesMap = (state) => {
-    console.log('category.Selector - selector fired');
-    return state.categories.categories.reduce((acc, category) => {         // querySnapshot.docs gives us an array of snapshots, we reduce over the querySnapShots.docs inorder to get the array structure of categories with their respected items
-     const { title, items } = category;                                // destructuring title and items from the docSnapshot (this grabs the data from the snapshot and grabs title and items from it)
-     acc[title.toLowerCase()] = items;                                           // the accumalator at the title value will be equal to the items, where, acc[index of item]
-        return acc;                                                                 // returns the accumalator    
- }, {});
-}
+import { createSelector } from "reselect";
+
+ const selectCategoryReducer = (state) => {
+    console.log('category.Selector - Selector 1 fired');
+    return state.categories;
+};
+
+export const selectCategories = createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice) => {
+        console.log('category.Selector - Selector 2 fired');
+        return categoriesSlice.categories;
+    }
+);
+
+export const selectCategoriesMap = createSelector(
+    [selectCategories],
+    (categories) => {
+        console.log('category.Selector - Selector 3 fired');
+        return categories.reduce((acc, category) => {         // querySnapshot.docs gives us an array of snapshots, we reduce over the querySnapShots.docs inorder to get the array structure of categories with their respected items
+        const { title, items } = category;                                // destructuring title and items from the docSnapshot (this grabs the data from the snapshot and grabs title and items from it)
+        acc[title.toLowerCase()] = items;                                           // the accumalator at the title value will be equal to the items, where, acc[index of item]
+           return acc;                                                                 // returns the accumalator    
+        }, {});
+    }
+);
